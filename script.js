fc6b80b9fb65042983be79423db9c66dc7ff43a0
Vijay Dhanik <vijaydhanik2@gmail.com>
2026-08-02 23:29:14 +0530

    Add mobile menu toggle functionality
    Implement mobile menu toggle functionality with open and close buttons.

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click" , () =>{
+    //Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+//close menu when the close button is clicked
+
+menuCloseButton.addEventListener("click",() => menuOpenButton.click());
